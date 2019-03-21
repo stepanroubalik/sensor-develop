0e754f75109a5ee40b376934b3d9850698d367b0
Štěpán Roubalík <stepan.roubalik93@gmail.com>
2019-03-21 14:35:25 +0100

    php soubor na renderování rastru

---
+++ render_raster.php
@@ -0,0 +1,19 @@
+<?php
+/** contents of test_raster.php **/
+$conn_str ='dbname=snimek host=localhost port=5432 user=postgres password=XXX';
+$dbconn = pg_connect($conn_str);
+header('Content-Type: image/png');
+/**If a particular projection was requested use it otherwise use mass state plane meters **/
+if (!empty( $_REQUEST['srid'] ) && is_numeric( $_REQUEST['srid']) ){
+		$input_srid = intval($_REQUEST['srid']);
+}
+else { $input_srid = 26986; }
+/** The set bytea_output may be needed for PostgreSQL 9.0+, but not for 8.4 **/
+$sql = "set bytea_output='escape';
+SELECT ST_AsPNG(rast) As rastpng FROM foto WHERE rid=1";
+$result = pg_query($sql);
+$row = pg_fetch_row($result);
+pg_free_result($result);
+if ($row === false) return;
+echo pg_unescape_bytea($row[0]);
+?>
