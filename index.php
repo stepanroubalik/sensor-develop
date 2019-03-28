84ed4d2e2c6d96487c6c09a491ffa09ef43269b2
Štěpán Roubalík <stepan.roubalik93@gmail.com>
2019-03-28 02:34:41 +0100

    Tvorba frontendu aplikace (editace souboru index.php)

--- index.php
+++ index.php
@@ -1,152 +1,117 @@
 <!DOCTYPE html>
 <html lang='cs'>
-  <head>
-    <title>VEG</title>
-    <meta charset='utf-8'>
-    <meta name='description' content=''>
-    <meta name='keywords' content=''>
-    <meta name='author' content=''>
-    <meta name='robots' content='all'>
-    <!-- <meta http-equiv='X-UA-Compatible' content='IE=edge'> -->
-    <!--leaflet & jquery-->
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css" integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ==" crossorigin=""/>
-    <script src="https://unpkg.com/leaflet@1.0.1/dist/leaflet.js"></script>
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"/>
-    <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
-    <!--Bootstrap a css soubory-->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style.css">
-    <!--Zoomhome plugin-->
-    <link rel="stylesheet" href="./zoomhome/dist/leaflet.zoomhome.css"/>
-    <script src="./zoomhome/dist/leaflet.zoomhome.min.js"></script>
-      
-    <!--sidebar style-->
-    <link rel="stylesheet" href="./css/leaflet-sidebar.css" />  
-    <script src="./js/leaflet-sidebar.js"></script>
-  </head>
+    <head>
+        <title>DIPLOMOVÁ PRÁCE</title>
+        <meta charset='utf-8'>
+        <meta name='description' content=''>
+        <meta name='keywords' content=''>
+        <meta name='author' content=''>
+        <meta name='robots' content='all'>
+        <!-- <meta http-equiv='X-UA-Compatible' content='IE=edge'> -->
+        <!--leaflet & jquery-->
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css" integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ==" crossorigin=""/>
+        <script src="https://unpkg.com/leaflet@1.0.1/dist/leaflet.js"></script>
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"/>
+        <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+        <!--Bootstrap a css soubory-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css">
+        <!--Zoomhome plugin-->
+        <link rel="stylesheet" href="./zoomhome/dist/leaflet.zoomhome.css"/>
+        <script src="./zoomhome/dist/leaflet.zoomhome.min.js"></script>
 
-  <body>
-      <!--<div id="side_panel" class="col-md-3">
-     
-          
-          <br>
-          <h5>Senzorová data</h5>
-          <label>Čas záznamu</label>
-          <input type="date" name="datum" value=""><br>
-          <label>Zobrazit v mapě</label>
-          <button id="filterSubmit">OK</button><br>
-          <label>Zobrazit v tabulce</label>
-          <button id="filterSubmit">OK</button>
-          
-          <h5>Zobrazení rastrových dat</h5>
-          <label>Čas záznam</label>
-          <input type="date" name="datum" value=""><br>
-          <label>Zobrazit v mapě</label>
-          <button id="filterSubmit">OK</button><br>
-          <label>Zobrazit v tabulce</label>
-          <button id="filterSubmit">OK</button>
-          
-          <h5>Výpočet vegetačních indexů</h5>
-          <label>Čas záznamu</label>
-          <input type="date" name="datum" value=""><br>
-          <label>Zobrazit v mapě</label>
-          <button id="filterSubmit" class="btn">OK</button><br>
-          <label>Zobrazit v tabulce</label>
-          <button id="filterSubmit" class="btn">OK</button>
-          
-          <h5>Korekční výpočet</h5>
-          <label>Čas záznamu</label>
-          <input type="date" name="datum" value=""><br>
-          <label>Zobrazit v mapě</label>
-          <button id="filterSubmit" class="btn">OK</button><br>
-          <label>Zobrazit v tabulce</label>
-          <button id="filterSubmit" class="btn">OK</button>
-          -->
-      </div>
-      <div id="sidebar" class="sidebar collapsed">
-        <!-- Nav tabs -->
-        <div class="sidebar-tabs">
-            <ul role="tablist">
-                <li><a href="#home" role="tab"><i class="fa fa-bars"></i></a></li>
-                <li><a href="#profile" role="tab"><i class="fa fa-user"></i></a></li>
-                <li class="disabled"><a href="#messages" role="tab"><i class="fa fa-envelope"></i></a></li>
-                <li><a href="https://github.com/Turbo87/sidebar-v2" role="tab" target="_blank"><i class="fa fa-github"></i></a></li>
-            </ul>
+        <!--sidebar style-->
+        <link rel="stylesheet" href="./css/leaflet-sidebar.css" />  
+        <script src="./js/leaflet-sidebar.js"></script>
+    </head>
+    <body>
+        <div id="sidebar" class="sidebar collapsed">
+            <!-- Nav tabs -->
+            <div class="sidebar-tabs">
+                <ul role="tablist">
+                    <li><a href="#home" role="tab"><i class="fa fa-file-image-o"></i></a></li>
+                    <li><a href="#profile" role="tab"><i class="fa fa-wifi"></i></a></li>
+                    <li><a href="#messages" role="tab"><i class="fa fa-gears"></i></a></li>
+                    <li><a href="https://github.com/stepanroubalik/sensor-develop" role="tab" target="_blank"><i class="fa fa-envelope"></i></a></li>
+                </ul>
 
-            <ul role="tablist">
-                <li><a href="#settings" role="tab"><i class="fa fa-gear"></i></a></li>
-            </ul>
+                <ul role="tablist">
+                    <li><a href="#settings" role="tab"><i class="fa fa-question-circle"></i></a></li>
+                </ul>
+            </div>
+
+            <!-- Tab panes -->
+            <div class="sidebar-content">
+                <div class="sidebar-pane" id="home">
+                        <h1 class="sidebar-header">
+                            Zobrazení rastrových dat
+                            <span class="sidebar-close"><i class="fa fa-caret-left"></i></span>
+                        </h1>
+                            <br><label class="form-check-label">Zobrazit snámky v časovém období <input type="date" id="datum" value="2019-03-30"></label>
+                        <div class="input-group mb-3">
+                            <h5><strong>Sentinel-2</strong></h5>
+                            <label class="form-check-label">
+                            <input type="radio" class="form-check-input" id="radio2" name="optradio" value="option2">RED (B04) <br>
+                            <input type="radio" class="form-check-input" id="radio2" name="optradio" value="option2">NIR (B08) <br>
+                            <input type="radio" class="form-check-input" id="radio2" name="optradio" value="option2">SWIR (B11) <br>
+                            </label>
+
+                            <h5><strong>Landsat 7</strong></h5>
+                            <label class="form-check-label">
+                            <input type="radio" class="form-check-input" id="radio2" name="optradio" value="option2">RED (B04) <br>
+                            <input type="radio" class="form-check-input" id="radio2" name="optradio" value="option2">NIR (B08) <br>
+                            <input type="radio" class="form-check-input" id="radio2" name="optradio" value="option2">SWIR (B11)
+                            </label>
+
+                            <h5><strong>Landsat 8</strong></h5>
+                            <label class="form-check-label">
+                            <input type="radio" class="form-check-input" id="radio2" name="optradio" value="option2">RED (B04) <br>
+                            <input type="radio" class="form-check-input" id="radio2" name="optradio" value="option2">NIR (B08) <br>
+                            <input type="radio" class="form-check-input" id="radio2" name="optradio" value="option2">SWIR (B11)
+                            </label>
+                        </div>
+                        <div class="btn-group">
+                            <button id="filterSubmit" class="btn btn-primary btn-sm btn-block">Načíst metadata</button>
+                            <button id="filterSubmit" class="btn btn-primary btn-sm btn-block">Import z DB</button>
+                            <button id="filterSubmit" class="btn btn-primary btn-sm btn-block">Zobrazit rastr</button>
+                            <button id="filterSubmit" class="btn btn-primary btn-sm btn-block">převést na 8-bitový rastr</button>
+                            <button id="filterSubmit" class="btn btn-primary btn-sm btn-block">převést na 16-bitový rastr</button>
+                        </div>
+                    </div>
+                <div class="sidebar-pane" id="profile">
+                        <h1 class="sidebar-header">
+                            Zobrazení senzorových dat
+                            <span class="sidebar-close"><i class="fa fa-caret-left"></i></span>
+                        </h1>
+          
+                        <div>
+                            <br>
+                            <label class="form-check-label">Zobrazit senzory časovém období <input type="date" id="datum" value="2019-03-30"></label>
+                            <label class="form-check-label">
+                            <input type="radio" class="form-check-input" id="radio2" name="optradio" value="option2">typ 1 <br>
+                            <input type="radio" class="form-check-input" id="radio2" name="optradio" value="option2">typ 2 <br>
+                            <input type="radio" class="form-check-input" id="radio2" name="optradio" value="option2">typ 3 <br>
+                            <input type="radio" class="form-check-input" id="radio2" name="optradio" value="option2">typ 4
+                            </label>
+                        </div>
+                        <div class="btn-group">
+                            <button id="filterSubmit" class="btn btn-primary btn-sm btn-block">Zobrazit data v tabulce</button>
+                            <button id="filterSubmit" class="btn btn-primary btn-sm btn-block">Odstranit tabulku </button>
+                            <button id="filterSubmit" class="btn btn-primary btn-sm btn-block">Zobrazit data v mapě</button>
+                            <button id="filterSubmit" class="btn btn-primary btn-sm btn-block">Odstranit z mapy</button>
+                        </div>
+                    </div>
+                <div class="sidebar-pane" id="messages">
+                <h1 class="sidebar-header">Výpočty<span class="sidebar-close"><i class="fa fa-caret-left"></i></span></h1>
+            </div>
+                <div class="sidebar-pane" id="settings">
+                <h1 class="sidebar-header">Settings<span class="sidebar-close"><i class="fa fa-caret-left"></i></span></h1>
+                </div>
+            </div><!---->
         </div>
-
-        <!-- Tab panes -->
-        <div class="sidebar-content">
-            <div class="sidebar-pane" id="home">
-                <h1 class="sidebar-header">
-                    Zobrazení rastrových a sensorových dat
-                    <span class="sidebar-close"><i class="fa fa-caret-left"></i></span>
-                </h1>
-                <h5>Senzorová data</h5>
-    <div>
-        <input type="date" id="datum" value="2019-03-30"><br>
-                    <select id="typ">
-                        <option value="A">A</option>
-                        <option value="B">B</option>
-                        <option value="C">C</option>            
-                    </select><br>
-                    <button id="filterSubmit">Zobraz Tabulku</button>
-                    <hr>
-      </div>
-      <div>
-        <input type="date" id="datum" value="2019-03-30"><br>
-                    <select id="typ">
-                        <option value="A">A</option>
-                        <option value="B">B</option>
-                        <option value="C">C</option>            
-                    </select><br>
-                    <button id="filterData">Zobraz sensory</button>
-                    <hr>
-      </div>
-    
-        
-    <script>
-        $("#filterSubmit").click(function(){
-            $.ajax({
-                url:'query_sensor_table.php',
-                type:'POST',
-                data:{
-                    datum: $("#datum").val(),
-                    typ: $("#typ").val()
-                },
-                success: function(response){
-                    $("#resultTable").html(response);
-                }
-            });
-        });
-    </script>
-      
-                
-
-              
-                
-            </div>
-
-        <div class="sidebar-pane" id="profile">
-                <h1 class="sidebar-header">Zpracování dat<span class="sidebar-close"><i class="fa fa-caret-left"></i></span></h1>
-            </div>
-            
-
-            <div class="sidebar-pane" id="messages">
-                <h1 class="sidebar-header">Messages<span class="sidebar-close"><i class="fa fa-caret-left"></i></span></h1>
-            </div>
-
-            <div class="sidebar-pane" id="settings">
-                <h1 class="sidebar-header">Odkazy<span class="sidebar-close"><i class="fa fa-caret-left"></i></span></h1>
-            </div>
-        </div>-->
-    </div>
-      <div id="mapdiv" class="col-md-12"></div>
         <div id="resultTable"></div>
-          <script>
+        <div id="mapdiv" class="col-md-12"></div>
+        <script>
             var map = L.map('mapdiv', { zoomControl:false });
             map.setView([49.5938686, 17.2508706], 12);
               
@@ -209,6 +174,5 @@
             L.control.scale({imperial:false, position:'bottomright'}).addTo(map);
             L.control.layers(baseLayers, overlays, {collapsed:true}).addTo(map);  
           </script>
-      </body>
-     
+    </body>
 </html>
