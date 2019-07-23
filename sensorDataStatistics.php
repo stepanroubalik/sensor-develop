474c7d7a845d6f0e76e5b04903f673febb1dc196
Štěpán Roubalík <stepan.roubalik@asseco-ce.com>
2019-07-23 16:25:53 +0200

    tvorba php souboru sensorDataStatistics.php

--- sensorDataStatistics.php
+++ sensorDataStatistics.php
@@ -8,10 +8,25 @@
 									FROM $sensorType  
 									WHERE ta <> 0 AND timestamp::text LIKE '$datetime%';");
 	$sqlMedian->execute();
-    	
+	
+	$sqlMean = $dbsensor->prepare("SELECT to_char(AVG (ta), '99999999999999999D999') AS průměr FROM $sensorType WHERE timestamp::text LIKE '$datetime%';");
+	$sqlMean->execute();
+    
+	echo "<h5>Výsledné hodnoty</h5>";
 	echo "<table id= 'dtHorizontalVerticalExample' class= 'table table-striped table-bordered table-sm' cellspacing='0', width= '100%'>";
     echo "<tr><th>median</th></tr>";
     while ($row = $sqlMedian->fetch(PDO::FETCH_ASSOC)){
+        echo "<tr>";
+        foreach($row as $field=>$value) {
+            echo "<td>{$value}</td>";
+        }
+        echo "</tr>";
+    }
+    echo"</table>";
+	
+	echo "<table id= 'dtHorizontalVerticalExample' class= 'table table-striped table-bordered table-sm' cellspacing='0', width= '100%'>";
+    echo "<tr><th>průměr</th></tr>";
+    while ($row = $sqlMean->fetch(PDO::FETCH_ASSOC)){
         echo "<tr>";
         foreach($row as $field=>$value) {
             echo "<td>{$value}</td>";
