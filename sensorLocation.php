581f41798dd3d5d71820329f585c26ce54567016
Štěpán Roubalík <stepan.roubalik@asseco-ce.com>
2019-07-25 09:35:56 +0200

    update sensor location

--- sensorLocation.php
+++ sensorLocation.php
@@ -1,12 +1,12 @@
 <?php
-	$sensorType = $_POST['sensorType'];
+	$sens = $_POST['sens'];
 	/*$datetime = $_POST['datetime'];
 	$dbsensor = new PDO('pgsql:host=158.194.94.120;port=5432;dbname=sensor;', 'roubalik', 'dp_roubalik2019');
 	$sql = $dbsensor->prepare("SELECT id, ta, timestamp FROM $sensorType WHERE timestamp::text LIKE '$datetime%';");
 	$sql->execute();*/
 	
     $db = new PDO('pgsql:host=localhost;port=5432;dbname=diplomka;', 'postgres', 'sr');
-    $sql = $db->prepare("SELECT coord As coord FROM $sensorType;");
+    $sql = $db->prepare("SELECT coord As coord FROM $sens;");
     $sql->execute();
 	    
     /*echo "<table id= 'dtHorizontalVerticalExample' class= 'table table-striped table-bordered table-sm' cellspacing='0', width= '100%'>";
