1beb503be4dffde5cf32e34fd5c4f661caca2588
Štěpán Roubalík <stepan.roubalik@asseco-ce.com>
2019-08-04 22:03:46 +0200

    parametry výpočtu indexu

--- vegetationIndicesNdvi.php
+++ vegetationIndicesNdvi.php
@@ -1,12 +1,12 @@
 <?php
-    $redpasmo = $_POST['ndvi_red'];
-    $nirpasmo = $_POST['ndvi_nir'];
+    $redpasmo = $_POST['ndviRed'];
+    $nirpasmo = $_POST['ndviNir'];
 	$ndvi = $_POST['ndvi'];
     $db = new PDO('pgsql:host=localhost;port=5432;dbname=diplomka;', 'postgres', 'sr');
     $deleteNdviTable = $db->prepare("DROP TABLE IF EXISTS $ndvi;");
 	$deleteNdviTable->execute();	
 	$sql = $db->prepare("CREATE TABLE $ndvi AS SELECT ST_MapAlgebra
-					(arast, 1, brast, 1, '(([rast1] - [rast2]) / ([rast1] + [rast2]))::float', '32BF')
+					(arast, 1, brast, 1, '(-1*([rast1] - [rast2]) / ([rast1] + [rast2]))::float', '32BF')
 					AS rast FROM 
 					(SELECT a.rast as arast, b.rast as brast FROM $redpasmo a INNER JOIN $nirpasmo b ON a.rid = b.rid) as joined");
     $sql->execute();
